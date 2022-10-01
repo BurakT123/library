ea82845908beb9e6a3d70d187b7221c1399c1de2
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2022-10-01 20:36:31 +0300

    Create ascii-binary-converter.js

---
+++ ascii-binary-converter.js
@@ -0,0 +1,21 @@
+function asciiToBinary(letter, toBase = 2, output = "") {
+    const asciiCode = letter.slice(0) && !letter.slice(1) ? letter.codePointAt(0) : null
+    if (toBase <= 1) return "Error: An integer less than two cannot be used in this conversion."
+
+    const division = (code) => {
+        if (code < toBase) return `${code}${output}`;
+        output = `${Math.floor(code % toBase)}${output}`
+        return division(Math.floor(code / toBase))
+    }
+
+    const binaryCode = (code) => {
+        if (code.split("").length == 7) return `0${code}`
+        return code
+    }
+
+    return { letter, asciiCode, binaryCode: binaryCode(division(asciiCode)) }
+}
+
+console.log(asciiToBinary("a"))
+
+//Online Test: https://runkit.com/cihatksm/ascii-binary
