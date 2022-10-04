56904f74c0820ebdc78ebc1f9fe6088e58b2d214
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2022-10-04 21:57:49 +0300

    Update ascii-binary-converter.js

--- ascii-binary-converter.js
+++ ascii-binary-converter.js
@@ -1,5 +1,10 @@
-function asciiAndBinary(letter, toBase = 2, output = "") {
-    const asciiCode = letter.slice(0) && !letter.slice(1) ? letter.codePointAt(0) : null
+const asciiCode = (letter) => {
+    if (!letter.slice(0)) return undefined;
+    if (letter.slice(1)) return `This is not a latter, it is a word or words`;
+    return String(letter.codePointAt(0))
+}
+
+function asciiToBinary(letter, toBase = 2, output = "") {
     if (toBase <= 1) return "Error: An integer less than two cannot be used in this conversion."
 
     const division = (code) => {
@@ -8,14 +13,19 @@ function asciiAndBinary(letter, toBase = 2, output = "") {
         return division(Math.floor(code / toBase))
     }
 
+    const putZero = (number, zeros = []) => {
+        for (let index = 0; index < number; index++) zeros.push("0")
+        return `${zeros.map(m => m).join("")}`
+    }
+
     const binaryCode = (code) => {
-        if (code.split("").length == 7) return `0${code}`
+        if (code.split("").length < 8) return `${putZero(8 - code.split("").length)}${code}`
         return code
     }
 
-    return { letter, asciiCode, binaryCode: binaryCode(division(asciiCode)) }
+    return { letter, asciiCode: asciiCode(letter), binaryCode: binaryCode(division(asciiCode(letter))) }
 }
 
-console.log(asciiAndBinary("a"))
+console.log(asciiToBinary("0"))
 
 //Online Test: https://runkit.com/cihatksm/ascii-binary
