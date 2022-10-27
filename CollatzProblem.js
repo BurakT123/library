3e23e98326e35d610300c2dd87f701e10fca0bf7
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2022-10-27 17:11:26 +0300

    Update CollatzProblem.js

--- CollatzProblem.js
+++ CollatzProblem.js
@@ -1,25 +1,16 @@
 //https://en.wikipedia.org/wiki/Collatz_conjecture
 //https://github.com/CihatKsm/library/blob/main/CollatzProblem.js
 
-const logColors = ['\x1b[31m%s\x1b[0m', '\x1b[32m%s\x1b[0m', '\x1b[33m%s\x1b[0m', '\x1b[34m%s\x1b[0m', '\x1b[35m%s\x1b[0m', '\x1b[36m%s\x1b[0m', '\x1b[33m%s\x1b[0m', '\x1b[32m%s\x1b[0m', '\x1b[36m%s\x1b[0m', '\x1b[31m%s\x1b[0m']
+function Collatz(number = 1, count = 1, arr = []) {
+    number % 2 == 0 ? arr.push(number / 2) : arr.push(3 * number + 1)
+    let count = (number) => arr.filter(x => x == number)?.length || 0
 
-const log = (text) => console.log(logColors[Math.floor(Math.random() * 10)], text)
-
-function Collatz(number = 1, count = 1, datas = []) {
-    if (number % 2 == 0) datas.push(number / 2)
-    else datas.push(3 * number + 1)
-
-    let foursCount = datas.filter(data => data == 4)?.length || 0
-    let twosCount = datas.filter(data => data == 2)?.length || 0
-    let onesCount = datas.filter(data => data == 1)?.length || 0
-
-    if (foursCount + twosCount + onesCount == 3) {
-        log(`> Number is ${count}. \n> Datas Length ${datas?.length || "0"} \n> ` +  datas.map(m => String(m)).join(" - ") + " \n")
-        datas = []
-        return setTimeout(() => Collatz(count + 1, count + 1, datas), 300);
+    if (count(4) + count(2) + count(1) == 3) {
+        console.log(`> Number is ${count}. \n` + `> Array Length ${arr?.length || "0"} \n` + `> ${arr.map(m => String(m)).join(" - ")} \n`)
+        return setTimeout(() => Collatz(count + 1, count + 1, []), 300);
     }
 
-    return setTimeout(() =>  Collatz(datas[(datas.length || 1) - 1], count, datas), 100);
+    Collatz(arr[(arr?.length || 1) - 1], count, arr)
 }
 
 Collatz()
